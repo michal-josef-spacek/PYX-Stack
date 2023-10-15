4a856e2b7045c129cdea19abab0804e3b3faaf52
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-10-15 17:01:13 +0200

    Prepare for 0.06 version

--- Stack.pm
+++ Stack.pm
@@ -7,7 +7,7 @@ use Class::Utils qw(set_params);
 use Error::Pure qw(err);
 use PYX::Parser;
 
-our $VERSION = 0.05;
+our $VERSION = 0.06;
 
 # Constructor.
 sub new {
@@ -327,6 +327,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.05
+0.06
 
 =cut
--- t/PYX-Stack/02-version.t
+++ t/PYX-Stack/02-version.t
@@ -6,4 +6,4 @@ use Test::More 'tests' => 2;
 use Test::NoWarnings;
 
 # Test.
-is($PYX::Stack::VERSION, 0.05, 'Version.');
+is($PYX::Stack::VERSION, 0.06, 'Version.');
